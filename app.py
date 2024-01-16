44598004e48f274120660864e585c34c903faffb
CLINTONPAU <48243181+CLINTONPAU@users.noreply.github.com>
2024-01-16 23:34:30 +0300

    app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import streamlit as st
 
 # Load the dataset
 url = 'https://raw.githubusercontent.com/CLINTONPAU/streamlit-predictions/main/sales.csv'
-df = pd.read_csv(url)
-
+df = pd.read_csv(url, parse_dates=['Date'], index_col='Date')
 # Function to engineer features
 def engineered_features(df):
     # Make a copy to avoid tampering with the original dataset
